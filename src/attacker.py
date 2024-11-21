de441b726f51a2a6a666a1977a0f639b387b12f9
Gary Szekely <gary.szekely.jr@gmail.com>
2024-11-21 17:11:39 -0500

    combine server and attacker

--- src/attacker.py
+++ src/attacker.py
@@ -1,10 +1,17 @@
 import socket
 import struct
+import time
 
+from threading import Thread
 from typing import Tuple
 
 
-server = ('localhost', 4000)
+address = ('localhost', 4000)
+
+
+def encode(address: Tuple[str, int]) -> bytes:
+    host, port = address
+    return socket.inet_aton(host) + struct.pack('H', port)
 
 
 def decode(data: bytes) -> Tuple[str, int]:
@@ -20,8 +27,24 @@ def attacker(server):
         victim = decode(data)
 
         print('victim:', *victim)
-        
+
         s.sendto(b'hello victim', victim)
 
 
-attacker(server)
+def server(address):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(address)
+        _, attacker = s.recvfrom(1)
+        _, victim = s.recvfrom(1)
+        s.sendto(encode(victim), attacker)
+
+
+serve = Thread(target=server, args=(address,))
+attack = Thread(target=attacker, args=(address,))
+
+serve.start()
+time.sleep(1)
+attack.start()
+
+serve.join()
+attack.join()
