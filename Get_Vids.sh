a8953b958e5fba8a05c2a5995480d2bdd47a03ea
Gary Szekely <gary.szekely.jr@gmail.com>
2024-11-21 16:04:17 -0500

    src layout

--- Get_Vids.sh
+++
@@ -1,2 +0,0 @@
-sudo scp -i ~/Desktop/KeyHackUMass.pem ec2-user@ec2-18-216-151-152.us-east-2.compute.amazonaws.com:./domino/stolen_files/camrecording.mp4  ~/Desktop
-sudo scp -i ~/Desktop/KeyHackUMass.pem ec2-user@ec2-18-216-151-152.us-east-2.compute.amazonaws.com:./domino/stolen_files/screenrecording.mp4  ~/Desktop
