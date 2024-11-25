97289595b65ed019f8f53b26a879dbb6ca00b970
Gary Szekely <gary.szekely.jr@gmail.com>
2024-11-24 22:55:05 -0500

    direct message channel after rendevous connect

---
+++ src/peer.py
@@ -0,0 +1,33 @@
+import socket
+import struct
+import time
+
+from typing import Tuple
+from threading import Thread
+
+
+def decode(data: bytes) -> Tuple[str, int]:
+    host = socket.inet_ntoa(data[:4])
+    port = struct.unpack('H', data[4:])[0]
+    return host, port
+
+
+def send():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.sendto(b's', ('localhost', 5319))
+        peer = decode(s.recv(6))
+        while True:
+            data = input()
+            s.sendto(data.encode(), peer)
+
+
+def recv():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.sendto(b'r', ('localhost', 5319))
+        while True:
+            print(s.recv(1024).decode())
+
+
+if __name__ == '__main__':
+    Thread(target=send).start()
+    Thread(target=recv).start()
---
+++ src/server.py
@@ -0,0 +1,31 @@
+import socket
+import struct
+
+from typing import Tuple
+
+
+def encode(address: Tuple[str, int]) -> bytes:
+    host, port = address
+    return socket.inet_aton(host) + struct.pack('H', port)
+
+
+if __name__ == '__main__':
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind(('0.0.0.0', 5319))
+
+        sends = []
+        receives = []
+
+        for _ in range(4):
+            data, address = s.recvfrom(1)
+            if data == b's':
+                sends.append(address)
+            else:
+                receives.append(address)
+
+        peer_1_send, peer_2_send = sends
+        peer_1_recv, peer_2_recv = receives
+
+        if peer_1_send[0] == peer_1_recv[0] and peer_2_send[0] == peer_2_recv[0]:
+            s.sendto(encode(peer_2_recv), peer_1_send)
+            s.sendto(encode(peer_1_recv), peer_2_send)
