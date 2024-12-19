f425ac9795329ed93e5780adcf6e436badf4b755
Gary Szekely <gary.szekely.jr@gmail.com>
2024-12-19 18:12:01 -0500

    removal of hackathon scripts

--- src/peer.py
+++ src/peer.py
@@ -1,6 +1,5 @@
 import socket
 import struct
-import time
 
 from typing import Tuple
 from threading import Thread
@@ -8,13 +7,13 @@ from threading import Thread
 
 def decode(data: bytes) -> Tuple[str, int]:
     host = socket.inet_ntoa(data[:4])
-    port = struct.unpack('H', data[4:])[0]
+    port = struct.unpack("H", data[4:])[0]
     return host, port
 
 
 def send():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.sendto(b's', ('localhost', 5319))
+        s.sendto(b"s", ("localhost", 5319))
         peer = decode(s.recv(6))
         while True:
             data = input()
@@ -23,11 +22,11 @@ def send():
 
 def recv():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.sendto(b'r', ('localhost', 5319))
+        s.sendto(b"r", ("localhost", 5319))
         while True:
             print(s.recv(1024).decode())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Thread(target=send).start()
     Thread(target=recv).start()
